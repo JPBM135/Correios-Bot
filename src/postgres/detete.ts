a613b32f7f5000aec2c1e36c234d6dcd1a466588
JPBM135 <jpedrobm0@gmail.com>
2022-11-03 12:07:42 -0300

    chore(pg): remove try catches

--- src/postgres/detete.ts
+++ src/postgres/detete.ts
@@ -2,51 +2,41 @@ import { logger, container, kSQL } from '@yuudachi/framework';
 import type { Sql } from 'postgres';
 
 export async function deleteCode(code: string): Promise<boolean> {
-	try {
-		const sql = container.resolve<Sql>(kSQL);
+	const sql = container.resolve<Sql>(kSQL);
 
-		const results = await sql<[{ code: string }]>`
-			delete from correios_codes
-			where code = ${code}
-			returning code
-		`;
+	const results = await sql<[{ code: string }]>`
+		delete from correios_codes
+		where code = ${code}
+		returning code
+	`;
 
-		return results.length > 0;
-	} catch (error) {
-		logger.error(error);
-		return false;
-	}
+	return results.length > 0;
 }
 
 export async function deleteUser(id: string): Promise<number> {
-	try {
-		const sql = container.resolve<Sql>(kSQL);
+	const sql = container.resolve<Sql>(kSQL);
 
-		const results = await sql<[{ id: string }]>`
-			delete from user_config
-			where user_id = ${id}
-			returning id
-		`;
+	const results = await sql<[{ id: string }]>`
+		delete from user_config
+		where user_id = ${id}
+		returning id
+	`;
 
-		if (!results.length) {
-			return 0;
-		}
-
-		const codes = await sql<[{ code: string }]>`
-			delete from correios_codes
-			where owner_id = ${id}
-			returning code
-		`;
-
-		logger.warn({
-			msg: 'Data delete',
-			user: id,
-			codes: codes.map((code) => code.code),
-		});
-
-		return codes.length;
-	} catch (error) {
-		logger.error(error);
+	if (!results.length) {
 		return 0;
 	}
+
+	const codes = await sql<[{ code: string }]>`
+		delete from correios_codes
+		where owner_id = ${id}
+		returning code
+	`;
+
+	logger.warn({
+		msg: 'Data delete',
+		user: id,
+		codes: codes.map((code) => code.code),
+	});
+
+	return codes.length;
 }
